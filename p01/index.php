783209459eb7d3e3ad483519950928387db3ebbb
c3ns <ernestas.radzvilas@gmail.com>
2018-04-10 14:52:48 +0300

    p01 homeworks

---
+++ p01/index.php
@@ -0,0 +1,26 @@
+<?php
+
+$type = "cat";
+$name = "Tim";
+$age = 1;
+$weight = 3.6;
+
+if(strtolower($name) === "tim" && strtolower($type) === "cat"){
+    echo "Tim is a cat";
+}else{
+    echo "Tim is not a cat";
+}
+
+echo "</br>---------------</br>";
+
+$price = 582;
+
+if($price > 500 && $price < 600){
+    echo "Price is big enough";
+}
+else if($price > 600 && $price < 700){
+    echo "OMG!";
+}
+else{
+    echo  "Price is ok";
+}
